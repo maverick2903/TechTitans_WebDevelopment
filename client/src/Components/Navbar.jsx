d7dcea038337ed570658223e54bdddab0a42ac28
msdhatrak25@gmail.com <msdhatrak25@gmail.com>
2023-02-11 19:05:26 +0530

    landing page styles

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,34 +1,75 @@
-import React from 'react'
-import {  useColorMode, Flex, Heading, HStack, Link, Spacer, IconButton } from '@chakra-ui/react'
-import {  SunIcon, MoonIcon } from '@chakra-ui/icons'
-import { NavLink } from 'react-router-dom'
-import {Navlinksx,menusx,iconsx} from "../Themes/sxThemes"
-import {titleLogosx} from "../Themes/sxThemes"
-import useAuth from "../Hooks/useAuth"
+import {
+    useColorMode,
+    Flex,
+    Heading,
+    HStack,
+    Link,
+    Spacer,
+    IconButton,
+} from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
+import { NavLink } from "react-router-dom";
+import { Navlinksx, titleLogosx, iconsx } from "../Themes/sxThemes";
+import useAuth from "../Hooks/useAuth";
+import Logo from "../assets/logo.png";
 
 export default function Navbar() {
+    const { Auth } = useAuth();
+    const { colorMode, toggleColorMode } = useColorMode();
 
-  const { Auth } = useAuth()
-  const { colorMode, toggleColorMode } = useColorMode()
+    return (
+        <Flex
+            sx={Navlinksx}
+            as="nav"
+            alignItems="center"
+            gap="10px"
+            my="9px"
+            height="5vh"
+            marginBottom="1rem"
+        >
+            <img
+                src={Logo}
+                alt="Website logo"
+                style={{ width: "3rem", marginLeft: "10px" }}
+            />
+            <Heading marginLeft="5px" sx={titleLogosx}>
+                GigGuru
+            </Heading>
+            <Spacer />
 
-  return (
-    <Flex sx={Navlinksx}  as="nav" alignItems="center" gap="10px" my="9px" height="5vh" >
-      <Heading marginLeft="18px" sx={titleLogosx}> TITLE </Heading>
-      <Spacer />
+            <HStack gap={5}>
+                {Auth?.user?.role === "client" && (
+                    <Link as={NavLink} to="/userpagerequest">
+                        Send Request
+                    </Link>
+                )}
+                {Auth?.user?.role === "worker" && (
+                    <Link as={NavLink} to="/userpagerequest">
+                        Client itneractions
+                    </Link>
+                )}
 
-      <HStack gap={5}>
+                <Link as={NavLink} to="/aboutus" style={{ fontSize: "2rem" }}>
+                    About us
+                </Link>
+                {!Auth && (
+                    <Link
+                        as={NavLink}
+                        to="/contactus"
+                        style={{ fontSize: "2rem" }}
+                    >
+                        Contact us
+                    </Link>
+                )}
 
-        {Auth?.user?.role=="client" && <Link as={NavLink} to="/userpagerequest">Send Request</Link>}
-        {Auth?.user?.role=="worker" && <Link as={NavLink} to="/userpagerequest">Client itneractions</Link>}
-
-        <Link as={NavLink}  to="/aboutus" >About us</Link>
-        {!Auth && <Link as={NavLink} to="/contactus">Contact us</Link>
-        }
-
-        <IconButton  sx={iconsx} onClick={()=>{toggleColorMode()}} icon={colorMode=="dark"?< SunIcon />:<MoonIcon />}> </IconButton>
-
-      </HStack>
-
-    </Flex>
-  )
+                <IconButton
+                    sx={iconsx}
+                    onClick={() => {
+                        toggleColorMode();
+                    }}
+                    icon={colorMode === "dark" ? <SunIcon /> : <MoonIcon />}
+                />
+            </HStack>
+        </Flex>
+    );
 }
--- client/src/Themes/sxThemes.jsx
+++ client/src/Themes/sxThemes.jsx
@@ -1,36 +1,35 @@
+export const Navlinksx = {
+    "font-size": { sm: "20px", md: "30px", lg: "35px", xl: "40px" },
+    "a:hover &": {
+        "text-decoration-color": "yellow",
+    },
+};
 
+export const iconsx = {
+    "font-size": { sm: "14px", md: "28px", lg: "33px", xl: "38px" },
+    height: { sm: "30px", md: "45px", lg: "53px", xl: "55px" },
+    width: { sm: "20px", md: "40px", lg: "53px", xl: "55px" },
+};
 
-export const Navlinksx={
-    "font-size": {sm:"20px",md:"30px",lg:"35px",xl:"40px"},
-    "a:hover &":{
-        "text-decoration-color":"yellow"
-    }
-}
+export const titleLogosx = {
+    "font-family": "Montserrat",
+    "font-size": { sm: "20px", md: "35px", lg: "42px", xl: "45px" },
+    "font-weight": "400",
+};
 
-export const iconsx={
-    "font-size": {sm:"14px",md:"28px",lg:"33px",xl:"38px"},
-    "height":{sm:"30px",md:"45px",lg:"53px",xl:"55px"},
-    "width":{sm:"20px",md:"40px",lg:"53px",xl:"55px"}
-}
+export const textsx = {
+    "font-family": "QuickSand",
+    "font-size": { sm: "20px", md: "28px", lg: "35px", xl: "40px" },
+};
 
-export const titleLogosx={
-    "font-family":'Shadows Into Light Two',
-    "font-size": {sm:"20px",md:"48px",lg:"53px",xl:"58px"}
-}
+export const textButtonsx = {
+    "font-family": "QuickSand",
+    "font-size": { sm: "20px", md: "22px", lg: "29px", xl: "40px" },
+};
 
-export const textsx={
-    "font-family":'QuickSand',
-    "font-size": {sm:"20px",md:"28px",lg:"35px",xl:"40px"}
-}
-
-export const textButtonsx={
-    "font-family":'QuickSand',
-    "font-size": {sm:"20px",md:"22px",lg:"29px",xl:"40px"}
-}
-
-export const menusx ={
-    "font-size": {sm:"20px",md:"25px",lg:"30px",xl:"36px"},
-}
+export const menusx = {
+    "font-size": { sm: "20px", md: "25px", lg: "30px", xl: "36px" },
+};
 
 /* export const backroundsx={
      "background-color":colorModeFunction=="dark"?"white":"pink"
