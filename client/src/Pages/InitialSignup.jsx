ab93f96709441640a14bbd2f56b6d4e36da683dd
Aman <aman2003nambisan@gmail.com>
2023-02-11 14:57:01 +0530

    added some styling for workersignup page

--- client/src/Pages/InitialSignup.jsx
+++ client/src/Pages/InitialSignup.jsx
@@ -10,67 +10,80 @@ import {
   RadioGroup,
 } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { textsx,textButtonsx } from "../Themes/sxThemes"
+import useAuth from "../Hooks/useAuth";
 
 function InitialSignup() {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [role, setRole] = useState("client");
+  const { Auth, setAuth } = useAuth();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const resp = await fetch("/user/newUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        role,
+      }),
+    });
+    //
+    const data = await resp.json();
+    if (resp.status === 401 || !data) {
+      window.alert("Invalid username/password"); //Can replace with a toast or popup
+    } else {
+      window.alert("Login Successful"); //Can replace with a toast or popup
+      setAuth({ user: data.username, role: data.role });
+    }
     if (role === "client") {
-      navigate("/signup/client");
+      navigate("/clientsignup");
     } else if (role === "worker") {
-      navigate("/signup/worker");
+      navigate("/workersignup");
     }
   };
+
   return (
-    <Box maxW="sm" borderWidth="1px" rounded="lg" p="6" m="auto" >
-      <form onSubmit={handleSubmit}>
-        <Stack spacing={4}>
-          <FormControl>
-            <FormLabel htmlFor="username">Username</FormLabel>
-            <Input
-              id="username"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </FormControl>
+      <Box  maxW="sm" borderWidth="1px" rounded="lg" p="6" m="auto" alignSelf="center" >
+        <form onSubmit={handleSubmit}>
+          <Stack spacing={4} sx={textsx} method="post">
+            <FormControl>
+              <FormLabel htmlFor="username">Username</FormLabel>
+              <Input
+                id="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </FormControl>
 
-          <FormControl>
-            <FormLabel htmlFor="password">Password</FormLabel>
-            <Input
-              id="password"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <Input
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </FormControl>
 
-          {/*           <FormControl>
             <FormLabel htmlFor="role">Role</FormLabel>
-            <Input
-              id="role"
-              type="text"
-              value={role}
-              onChange={(e) => setRole(e.target.value)}
-            />
-          </FormControl> */}
+            <RadioGroup id="role" onChange={setRole} value={role}>
+              <Stack direction="row">
+                <Radio value="client">Client</Radio>
+                <Radio value="worker">Worker</Radio>
+              </Stack>
+            </RadioGroup>
 
-          <FormLabel htmlFor="role">Role</FormLabel>
-          <RadioGroup id="role" onChange={setRole} value={role}>
-            <Stack direction="row">
-              <Radio value="client">Client</Radio>
-              <Radio value="worker">Worker</Radio>
-            </Stack>
-          </RadioGroup>
-
-          <Button type="submit">Sign up</Button>
-        </Stack>
-      </form>
-    </Box>
+            <Button type="submit">Sign up</Button>
+          </Stack>
+        </form>
+      </Box>
   );
 }
 
--- client/src/Pages/WorkerSignup.jsx
+++ client/src/Pages/WorkerSignup.jsx
@@ -7,7 +7,9 @@ import {
   Button,
   Stack,
   Text,
+  VStack,
 } from "@chakra-ui/react";
+import { textsx,textButtonsx } from "../Themes/sxThemes"
 
 const WorkerSignUp = () => {
   const [name, setName] = useState("");
@@ -20,44 +22,54 @@ const WorkerSignUp = () => {
   };
 
   return (
-    <Box maxW="sm" borderWidth="1px" rounded="lg" p="6" m="auto" textAlign="center">
-      <Text> You are almost done!</Text>
+    <div className="workersignup">
+    <Box maxW="md" width="50%" borderWidth="1px" border="2px solid" rounded="lg" p="6" m="auto" textAlign="center" >
+      <Text sx={textsx} mb="24px"> You are almost done!</Text>
       <form onSubmit={handleSubmit}>
-        <Stack spacing={4}>
-          <FormControl>
-            <FormLabel htmlFor="name">Name</FormLabel>
-            <Input
-              id="name"
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </FormControl>
+        <Stack spacing={4} sx={textsx}>
 
-          <FormControl>
-            <FormLabel htmlFor="contactNumber">Contact Number</FormLabel>
-            <Input
-              id="contactNumber"
-              type="text"
-              value={contactNumber}
-              onChange={(e) => setContactNumber(e.target.value)}
-            />
-          </FormControl>
+          <div className="parent">
+            <FormControl>
+              <FormLabel htmlFor="name">Name</FormLabel>
+              <Input
+                id="name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </FormControl>
+          </div>
 
-          <FormControl>
-            <FormLabel htmlFor="city">City</FormLabel>
-            <Input
-              id="city"
-              type="text"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-            />
-          </FormControl>
+          <div className="parent">
+            <FormControl>
+              <FormLabel htmlFor="contactNumber">Contact Number</FormLabel>
+              <Input
+                id="contactNumber"
+                type="text"
+                value={contactNumber}
+                onChange={(e) => setContactNumber(e.target.value)}
+              />
+            </FormControl>
+          </div>
 
-          <Button type="submit">Sign up</Button>
+          <div className="parent">
+            <FormControl>
+              <FormLabel htmlFor="city">City</FormLabel>
+              <Input
+                id="city"
+                type="text"
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
+            </FormControl>
+          </div>
+          <Box>
+            <Button m="auto" sx={textButtonsx} justifySelf="center" height={{ sm: "23px", md: "34px", lg: "43px", xl: "52px" }} width={{ sm: "150px", md: "180px", lg: "200px", xl: "230px" }} type="submit">Sign up</Button>
+          </Box>
         </Stack>
       </form>
     </Box>
+    </div>
   );
 };
 
