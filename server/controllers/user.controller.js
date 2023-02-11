3493bce5a5573128d91940908a1e29c7b7696f35
msdhatrak25@gmail.com <msdhatrak25@gmail.com>
2023-02-12 03:53:40 +0530

    fixed auth issue

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -8,57 +8,71 @@ app.use(express.json());
 const generateOtp = require("../utilities/utils");
 const SecretKey = process.env.SECRET_KEY;
 const newUser = async (req, res) => {
-  try {
-    const { username, password, role } = req.body;
-    if (!username || !password || !role)
-      return res
-        .status(400)
-        .json({ message: "Please fill the necessary details " });
-    const user = new User(req.body);
-    await user.save();
-    res.status(200).json({ message: "Successfully Registered" });
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+    try {
+        const { username, password, role } = req.body;
+        if (!username || !password || !role)
+            return res
+                .status(400)
+                .json({ message: "Please fill the necessary details " });
+        const user = new User(req.body);
+        await user.save();
+        const token = jwt.sign({ username: req.body.username }, SecretKey);
+        res.cookie("jsonwebtoken", token, {
+            // expires: new Date(Date.now() + 3600000),
+            maxAge: 604800000,
+            httpOnly: true,
+            sameSite: "none",
+            secure: true,
+        });
+        res.status(200).json({
+            message: "Successfully Registered",
+            user,
+            token,
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 };
 
 const loginUser = async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    if (!username || !password)
-      return res
-        .status(400)
-        .json({ message: "Please fill the necessary details " });
-    const userData = await User.findOne({ username: req.body.username });
-    if (!userData) return res.status(400).json({ message: "User not found" });
-    const validPassword = await bcrypt.compare(
-      req.body.password,
-      userData.password
-    );
-    if (!userData || !validPassword)
-      res.status(400).json({ message: "Invalid credentials" });
-    else {
-      const token = jwt.sign({ username: req.body.username }, SecretKey);
-      res.cookie("jsonwebtoken", token, {
-        expires: new Date(Date.now() + 3600000),
-        httpOnly: true,
-        sameSite: "none",
-        secure: true,
-      });
-      return res.status(200).json(userData);
+    try {
+        const { username, password } = req.body;
+        if (!username || !password)
+            return res
+                .status(400)
+                .json({ message: "Please fill the necessary details " });
+        const userData = await User.findOne({ username: req.body.username });
+        if (!userData)
+            return res.status(400).json({ message: "User not found" });
+        const validPassword = await bcrypt.compare(
+            req.body.password,
+            userData.password
+        );
+        if (!userData || !validPassword)
+            res.status(400).json({ message: "Invalid credentials" });
+        else {
+            const token = jwt.sign({ username: req.body.username }, SecretKey);
+            res.cookie("jsonwebtoken", token, {
+                // expires: new Date(Date.now() + 3600000),
+                maxAge: 604800000,
+                httpOnly: true,
+                sameSite: "none",
+                secure: true,
+            });
+            return res.status(200).json({ userData, token });
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
     }
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
 };
 
 const logout = async (req, res) => {
-  res.clearCookie("jsonwebtoken", { path: "/" });
-  res.status(200).json({ message: "User logged out successfully" });
+    res.clearCookie("jsonwebtoken", { path: "/" });
+    res.status(200).json({ message: "User logged out successfully" });
 };
 
 module.exports = {
-  newUser,
-  loginUser,
-  logout,
+    newUser,
+    loginUser,
+    logout,
 };
--- server/routes/worker.routes.js
+++ server/routes/worker.routes.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const router = new express.Router();
 
-const{
+const {
     newWorker,
     updateWorker,
     deleteWorker,
     selfView,
     clientJob,
     particularClientJob,
-    requestClient
-}=require('../controllers/worker.controller')
-const authenticate=require('../middleware/auth')
+    requestClient,
+} = require("../controllers/worker.controller");
+const authenticate = require("../middleware/auth");
 
-router.post('/newWorker',newWorker)
-router.patch('/updateWorker',authenticate,updateWorker)
-router.delete('/deleteWorker',authenticate,deleteWorker)
-router.get('/selfView',authenticate,selfView)
-router.get('/clientJob',authenticate,clientJob)
-router.get('/particularClientJob',authenticate,particularClientJob)
+router.post("/newWorker", newWorker);
+router.patch("/updateWorker", authenticate, updateWorker);
+router.delete("/deleteWorker", authenticate, deleteWorker);
+router.get("/selfView", authenticate, selfView);
+router.post("/clientJob", authenticate, clientJob);
+router.get("/particularClientJob", authenticate, particularClientJob);
 
-
-module.exports=router
+module.exports = router;
