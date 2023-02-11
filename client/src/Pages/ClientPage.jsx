aeec7567ff61b35cfd0b41c25cfcc1697963c824
Varun Viswanath <varunvis2903@gmail.com>
2023-02-12 04:14:29 +0530

    client page and chat

--- client/src/Pages/ClientPage.jsx
+++ client/src/Pages/ClientPage.jsx
@@ -7,14 +7,19 @@ import {
   Select,
   Button,
   Stack,
-  Radio,
-  RadioGroup,
+  Center,
   Text,
+  Flex,
+  Heading,
+  Image,
+  useBreakpointValue,
 } from "@chakra-ui/react";
+import { Navigate, useNavigate } from "react-router-dom";
+
 export default function ClientPage() {
+  const navigate = useNavigate();
   const [request, setRequest] = useState("");
   const [field, setField] = useState("");
-  console.log(field);
   const handleChange = (event) => {
     setField(event.target.value);
   };
@@ -23,7 +28,7 @@ export default function ClientPage() {
     e.preventDefault();
 
     //api login
-    const resp = await fetch("/client/request", {
+    const resp = await fetch("http://localhost:5000/client/request", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -33,8 +38,8 @@ export default function ClientPage() {
         category: field,
       }),
     });
-    //
     const data = await resp.json();
+    console.log(data);
     if (resp.status === 401 || !data) {
       window.alert("Invalid details"); //Can replace with a toast or popup
     } else {
@@ -43,34 +48,113 @@ export default function ClientPage() {
   };
 
   return (
-    <Box borderWidth="1px" rounded="lg" p="6" m="5em" textAlign="center">
-      <Text>New Request</Text>
-      <form method="POST" onSubmit={handleSubmit}>
-        <FormControl>
-          <FormLabel htmlFor="field">Select category of work</FormLabel>
-          <Select
-            placeholder="Select option"
-            id="field"
-            value={field}
-            onChange={handleChange}
-          >
-            <option value="Plumber">Plumber</option>
-            <option value="Electrician">Electrician</option>
-            <option value="Packers and Movers">Packers and Movers</option>
-            <option value="Housework">Housework</option>
-          </Select>
-        </FormControl>
-        <FormControl>
-          <FormLabel htmlFor="request">Post your request here</FormLabel>
-          <Input
-            id="request"
-            type="text"
-            value={request}
-            onChange={(e) => setRequest(e.target.value)}
+    <div>
+      <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+        <Flex p={8} flex={1} align={"center"} justify={"center"}>
+          <Stack spacing={6} w={"full"} maxW={"lg"}>
+            <Heading fontSize={{ base: "3xl", md: "4xl", lg: "5xl" }}>
+              <Text
+                as={"span"}
+                position={"relative"}
+                _after={{
+                  content: "''",
+                  width: "full",
+                  height: useBreakpointValue({ base: "20%", md: "30%" }),
+                  position: "absolute",
+                  bottom: 1,
+                  left: 0,
+                  bg: "blue.400",
+                  zIndex: -1,
+                }}
+              >
+                GigGuru
+              </Text>
+              <br />{" "}
+              <Text color={"blue.400"} as={"span"}>
+                gets it done
+              </Text>{" "}
+            </Heading>
+            <Text fontSize={{ base: "md", lg: "lg" }} color={"gray.500"}>
+              GigGuru is the perfect solution for all your daily work
+              requirements. We connect you to trusted people who'll do the job
+              you want to do.
+            </Text>
+            <Stack direction={{ base: "column", md: "row" }} spacing={4}>
+              <Button
+                rounded={"full"}
+                bg={"blue.400"}
+                color={"white"}
+                _hover={{
+                  bg: "blue.500",
+                }}
+              >
+                <a href="#newReq">Create New Request</a>
+              </Button>
+
+              <Button
+                onClick={() => navigate("/clientpagerequests")}
+                rounded={"full"}
+              >
+                Ongoing Requests
+              </Button>
+            </Stack>
+          </Stack>
+        </Flex>
+        <Flex flex={1}>
+          <Image
+            alt={"Login Image"}
+            objectFit={"cover"}
+            borderRadius="20px"
+            src={
+              "https://images.unsplash.com/photo-1527689368864-3a821dbccc34?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+            }
           />
-        </FormControl>
-        <Button colorScheme="blue">Button</Button>
-      </form>
-    </Box>
+        </Flex>
+      </Stack>
+      <div id="newReq">
+        <Stack>
+          <Box
+            borderWidth="1px"
+            rounded="lg"
+            p="6"
+            m="5em"
+            textAlign="center"
+            backgroundColor={"blue.400"}
+          >
+            <Text>New Request</Text>
+            <form method="POST" onSubmit={handleSubmit}>
+              <FormControl>
+                <FormLabel htmlFor="field">Select category of work</FormLabel>
+                <Select
+                  placeholder="Select option"
+                  id="field"
+                  value={field}
+                  onChange={handleChange}
+                >
+                  <option value="Plumber">Plumber</option>
+                  <option value="Electrician">Electrician</option>
+                  <option value="Packers and Movers">Packers and Movers</option>
+                  <option value="Housework">Housework</option>
+                </Select>
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="request">Post your request here</FormLabel>
+                <Input
+                  id="request"
+                  type="text"
+                  value={request}
+                  onChange={(e) => setRequest(e.target.value)}
+                />
+              </FormControl>
+              <Center>
+                <Button type="submit" marginTop="20px" colorScheme="green">
+                  Submit Request
+                </Button>
+              </Center>
+            </form>
+          </Box>
+        </Stack>
+      </div>
+    </div>
   );
 }
--- client/src/Pages/ClientPageRequests.jsx
+++ client/src/Pages/ClientPageRequests.jsx
@@ -1,7 +1,139 @@
-import React from 'react'
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  SimpleGrid,
+  Text,
+  Input,
+  FormLabel,
+  FormControl,
+} from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { useEffect, useState, useRef } from "react";
+
+function ClientListingCard({ clientData }) {
+  const [userName, setUserName] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [priceByWorker, setPriceByWorker] = useState();
+  const initialRef = useRef(null);
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <Heading size="md">{clientData.field}</Heading>
+        </CardHeader>
+        <CardBody>
+          <Text>{clientData.description}</Text>
+        </CardBody>
+        <CardFooter>
+          <Button onClick={onOpen}>Chat with worker</Button>
+        </CardFooter>
+      </Card>
+      <Modal
+        blockScrollOnMount={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        initialFocusRef={initialRef}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Negotiation</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* Here we need to add asection for all prices */}
+            <form method="POST">
+              <FormControl>
+                <FormLabel htmlFor="username">Quote a price</FormLabel>
+                <Input
+                  id="price"
+                  ref={initialRef}
+                  type="number"
+                  value={priceByWorker}
+                  onChange={(e) => setPriceByWorker(e.target.value)}
+                />
+              </FormControl>
+            </form>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant="ghost">Confirm new price</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
 
 export default function ClientPageRequests() {
+  const [clientListing, setClientListing] = useState([
+    {
+      field: "Electrician",
+      description: "Need to fix 2 tubelights",
+      username: "mohit",
+    },
+    {
+      field: "Electrician",
+      description: "Need to fix 2 tubelights",
+      username: "rohit",
+    },
+    {
+      field: "Electrician",
+      description: "Need to fix 2 tubelights",
+      username: "varun",
+    },
+    {
+      field: "Electrician",
+      description: "Need to fix 2 tubelights",
+      username: "karun",
+    },
+  ]);
+
+  // useEffect(() => {
+  //     (async () => {
+  //         // const resp = await fetch("http://localhost:5000/", {
+  //         //     method: "GET",
+  //         //     headers: {
+  //         //         "Content-Type": "application/json",
+  //         //     },
+  //         // });
+  //         // //
+  //         // const data = await resp.json();
+  //         // if (resp.status === 200) {
+  //         //     //Can replace with a toast or popup
+  //         //     console.log("login done");
+  //         //     // setAuth({ user: data.username, role: data.role });
+  //         //     // console.log(auth);
+  //         //     // navigate(from, { replace: true });
+  //         // } else {
+  //         //     window.alert("Invalid username/password"); //Can replace with a toast or popup
+  //         // }
+  //     })();
+  // }, []);
+
   return (
-    <div>ClientPageRequests</div>
-  )
+    <SimpleGrid
+      marginTop="3rem"
+      spacing={4}
+      templateColumns="repeat(auto-fill, minmax(200px, 1fr))"
+    >
+      {clientListing.map((clientData) => (
+        <ClientListingCard clientData={clientData} key={clientData.username} />
+      ))}
+    </SimpleGrid>
+  );
 }
